8f25bb17ac15a10d4688f2079e3c787389eb3a23
Sony Theakanath <stheakanath@gmail.com>
2012-07-30 15:24:53 -0700

    Initial Commit

---
+++ MultipleSocialNetworksUpdate/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  MultipleSocialNetworksUpdate
+//
+//  Created by Thomas Theakanath on 7/30/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ MultipleSocialNetworksUpdate/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MultipleSocialNetworksUpdate
+//
+//  Created by Thomas Theakanath on 7/30/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
